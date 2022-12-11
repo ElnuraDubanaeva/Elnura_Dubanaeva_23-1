ab72eb68957e0154da74e591a0e1953ea205398e
ElnuraDubanaeva <elena040203@gmail.com>
2022-12-11 23:32:55 +0600

    'hw1'

--- posts/views.py
+++ posts/views.py
@@ -15,10 +15,6 @@ def now_time(request):
     return HttpResponse(f"{datetime.date.today()}")
 
 
-def admin(request):
-    return HttpResponse(f'{request.method}',
-                        {request.path})
-
 
 def goodby(request):
     return HttpResponse('Goodby user!')
